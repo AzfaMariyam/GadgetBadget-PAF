9032100ab625f00ab20557809bebfd05f763861e
tharushika1996 <tharushikamaddumarachchi@gmail.com>
2021-04-21 10:01:43 +0530

    Add changes to buyer

--- BuyerManagement/src/com/BuyerService.java
+++ BuyerManagement/src/com/BuyerService.java
@@ -1,5 +1,25 @@
 package com;
 
-public class BuyerService {
-
+import model.Buyer; 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+@Path("/Items") 
+public class BuyerService 
+{ 
+ Buyer itemObj = new Buyer(); 
+@GET
+@Path("/") 
+@Produces(MediaType.TEXT_HTML) 
+public String readItems() 
+ { 
+ return "Hello"; 
+ } 
 }
+
