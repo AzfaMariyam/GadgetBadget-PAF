c71433f37bfe6b75d0eca8afe579d71ca6ec71e1
sewmawee <sewwandimawee.gmail.com>
2021-04-21 18:09:13 +0530

    read funders operation done

--- FundersManagement/src/com/funderService.java
+++ FundersManagement/src/com/funderService.java
@@ -1,6 +1,41 @@
 package com;
 
+
+
+import model.funder;
+
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Funders")
+
+
 public class funderService {
+	
+	
+	
+	funder funderObj = new funder(); 
+	
+	
+	//Reading
+	//completed projects
+	
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String readFunders() 
+	 { 
+	 return funderObj.readFunders();
+	 } 
 	
 
 }
--- FundersManagement/src/model/funder.java
+++ FundersManagement/src/model/funder.java
@@ -14,7 +14,7 @@ public class funder {
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			//Provide the correct details: DBServer/DBName, username, password
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/lab_3.1","root", "");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget","root", "");
 			
 			//for testing
 			System.out.println("Successfully connected");
@@ -30,56 +30,10 @@ public class funder {
 	
 	
 	
-	
-	
-	public String insertFunderDetails(String code , String name , String price , String desc) {
+	//READING
 		
-		String output = "";
-		
-		try {
-			
-			Connection con = connect();
-			
-			if(con == null) {
-				return "Error while connecting to the database for inserting";
-			}
-			
-			//create a prepared statement
-			String query = " insert into items(itemID,itemCode,itemName,itemPrice,itemDesc)" 
-			+ " values(?,?,?,?,?)";
-			
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
-			//binding values
-			preparedStmt.setInt(1,0);
-			preparedStmt.setString(2,code);
-			preparedStmt.setString(3,name);
-			preparedStmt.setDouble(4,Double.parseDouble(price));
-			preparedStmt.setString(5,desc);
-			
-			//execute the statement
-			preparedStmt.execute();
-			con.close();
-			
-			output = "Inserted successfully";
-		}
-		catch(Exception e) {
-			
-			output = "Error while inserting";
-			System.err.println(e.getMessage());
-			
-		}
-		
-		return output;
-		
-	}
-	
-	
-	
-	
-	
-	
-	public String readFunderDetails() {
+	//Reading the projects that are completed
+	public String readFunders() {
 		
 		String output = "";
 		
@@ -92,214 +46,76 @@ public class funder {
 				return "Error while connecting to the database for the reading";
 				
 			}
-			
-			//prepare the html table to be displayed
-			output = "<table border='1'><tr><th>Item Code</th>"
-					+ "<th>Item Name</th><th>Item Price</th>"
-					+ "<th>Item Description</th>"
+			//,funderName,funderAddress,funderTel,funderEmail,funderGender,funderFund,funderTime,funderDes
+			//prepare the HTML table to be displayed
+			output = "<table border='1'><tr><th>Funder Code</th>"
+					+ "<th>Funder Name</th>"
+					+ "<th>Funder Address</th>"
+					+ "<th>Funder Tel</th>"
+					+ "<th>Funder Email</th>"
+					+ "<th>Funder Gender</th>"
+					+ "<th>Funder Fund</th>"
+					+ "<th>Funder Time</th>"
+					+ "<th>Funder Description</th>"
 					+ "<th>Update</th><th>Remove</th></tr>";
 			
-			String query = "select * from items";
+			String query = "select * from funders";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			
 			//iterate through the rows in the result set
 			while(rs.next()) {
 				
-				String itemID = Integer.toString(rs.getInt("itemID"));
-				String itemCode = rs.getString("itemCode");
-				String itemName = rs.getString("itemName");
-				String itemPrice = Double.toString(rs.getDouble("itemPrice"));
-				String itemDesc = rs.getString("itemDesc");
+				String funderID = Integer.toString(rs.getInt("funderID"));
+				String funderCode = rs.getString("funderCode");
+				String funderName = rs.getString("funderName");
+				String funderAddress = rs.getString("funderAddress");
+				String funderTel = rs.getString("funderTel");
+				String funderEmail = rs.getString("funderEmail");
+				String funderGender = rs.getString("funderGender");
+				String funderFund = Double.toString(rs.getDouble("funderFund"));
+				String funderTime = rs.getString("funderTime");
+				String funderDes = rs.getString("funderDes");
 				
-				//Add a row into the html table
-				output += "<tr><td>" + itemCode + "</td>";
-				output += "<td>" + itemName + "</td>";
-				output += "<td>" + itemPrice + "</td>";
-				output += "<td>" + itemDesc + "</td>";
+				
+				//Add a row into the HTML table
+				output += "<tr><td>" + funderCode + "</td>";
+				output += "<td>" + funderName + "</td>";
+				output += "<td>" + funderAddress + "</td>";
+				output += "<td>" + funderTel + "</td>";
+				output += "<td>" + funderEmail + "</td>";
+				output += "<td>" + funderGender + "</td>";
+				output += "<td>" + funderFund + "</td>";
+				output += "<td>" + funderTime + "</td>";
+				output += "<td>" + funderDes + "</td>";
 				
 				//buttons
-				output += "<td><form method='post' action='items.jsp'>"
+				output += "<td><form method='post' action='funders.jsp'>"
 						+ "<input name='btnUpdate' "
 						+ " type='submit' value='Update'>"
-						+ "<input name='itemID' type='hidden' "
-						+ " value='" + itemID + "'>" + "</form></td>"
+						+ "<input name='funderID' type='hidden' "
+						+ " value='" + funderID + "'>" + "</form></td>"
 						
-						+ "<td><form method='post' action='items.jsp'>"
+						+ "<td><form method='post' action='funders.jsp'>"
 						+ "<input name='btnRemove'"
 						+ " type='submit' value='Remove'>"
-						+ "<input name='itemID' type='hidden' "
-						+ " value='" + itemID + "'>" + "</form></td></tr>";
+						+ "<input name='funderID' type='hidden' "
+						+ " value='" + funderID + "'>" + "</form></td></tr>";
 			}
 			con.close();
 			
-			//Complete the html table
+			//Complete the HTML table
 			output += "</table>";
 		}
 		catch(Exception e) {
 			
-			output = "Error while reading the items.";
+			output = "Error while reading.";
 			System.err.println(e.getMessage());
 		}
 		
 		return output;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	public String updateFunderDetails(String ID, String code, String name, String price, String desc)
-	 { 
-		 String output = ""; 
-		 try
-		 { 
-			 Connection con = connect();
-			 
-			 if (con == null) 
-			 
-			 {return "Error while connecting to the database for updating."; } 
-		 
-			 // create a prepared statement
-			 String query = "UPDATE items SET itemCode=?,itemName=?,itemPrice=?,itemDesc=?"
-			 		+ " WHERE itemID=?";
-			 
-			 	
-			 		 		
-			 
-			 PreparedStatement preparedStmt = con.prepareStatement(query); 
-			 // binding values
-			 
-			 preparedStmt.setString(1, code); 
-			 preparedStmt.setString(2, name); 
-			 preparedStmt.setDouble(3, Double.parseDouble(price)); 
-			 preparedStmt.setString(4, desc); 
-			 preparedStmt.setInt(5, Integer.parseInt(ID)); 
-			 // execute the statement
-			 
-			 preparedStmt.execute(); 
-			 con.close(); 
-			 
-			 output = "Updated successfully"; 
-		 } 
-		 catch (Exception e) 
-		 { 
-			 output = "Error while updating the item."; 
-			 System.err.println(e.getMessage()); 
-		 } 
-		 return output; 
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public String removeFunderDetails(String Id) {
-		
-		String output = "";
-		
-		try {
-			
-			Connection con = connect();
-			
-			if(con == null) {
-				return "Error while connecting to the database for removing";
-			}
-			
-			//create a prepared statement
-			String query = " delete from items where itemID = " 
-			+ " ? ";
-			
-			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
-			//binding values
-			preparedStmt.setString(1,Id);
-			
-			
-			//execute the statement
-			preparedStmt.executeUpdate();
-			con.close();
-			
-			output = "Removed successfully";
-		}
-		catch(Exception e) {
-			
-			output = "Error while removing";
-			System.err.println(e.getMessage());
-			
-		}
-		
-		return output;
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public String[] getFunderDetails(String id ) {
-		
-		String[] item = {"","","",""};
-		
-		try {
-			
-			Connection con = connect();
-			
-			if(con == null) {
-				//return "Error while connecting to the database for updating";
-			}
-			
-			//create a prepared statement
-			String query = " select * from items where itemID = " + id;
-			
-			//PreparedStatement preparedStmt = con.prepareStatement(query);
-			
-			//binding values
-			//preparedStmt.setString(1,id);
-			Statement stmt = con.createStatement();
-			
-			
-			//execute the statement
-			ResultSet rs = stmt.executeQuery(query);
-			
-			
-			
-			while(rs.next()) {
-				
-			item[0] = rs.getString("itemCode");
-			item[1] = rs.getString("itemName");
-			item[2] = Double.toString(rs.getDouble("itemPrice"));
-			item[3] = rs.getString("itemDesc");
-			
-			}
-			
-			con.close();
-			
-			
-		}
-		catch(Exception e) {
-			
-			//output = "Error while updating";
-			System.err.println(e.getMessage());
-			
-		}
-		
-		return item;
-		
-	}
 	
 	
 	
