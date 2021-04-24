ff0dd192ce1e1c277b0c6b1da20252b30d9db9c7
AzfaMariyam <azfamariyam@gmail.com>
2021-04-24 17:32:06 +0530

    made changes to shipping details and card details

--- PaymentManagement/src/com/CardDetService.java
+++ PaymentManagement/src/com/CardDetService.java
@@ -1,5 +1,33 @@
 package com;
 
+import model.CardDetails;
+import model.Shipping;
+
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document;
+
+
+@Path("/CardDetails")
 public class CardDetService {
 
+	CardDetails cardD = new CardDetails();
+	
+	//read card details
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML) 
+	public String readCardDetails() 
+	{ 
+	 return cardD.readCardDetails();
+	}
+	
 }
